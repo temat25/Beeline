04e2bc0ff62752f73ede14979ee00f5ea46a425f
Temo Tsulukiani <temat25@gmail.com>
2016-04-01 15:03:07 +0400

    first commit

---
+++ app/src/main/java/com/example/ayaravoy/beeline/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ayaravoy.beeline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button PushMe;
+    TextView Android;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button PushMe = (Button) findViewById(R.id.btPushMe);
+        TextView Android = (TextView) findViewById(R.id.tvAndroid);
+    }
+}
